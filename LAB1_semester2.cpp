87d3947ef20deeca79028fcde898f77121c7619f
Roman Mishchuk <roman.mishchuk@tum.de>
2023-03-29 02:50:33 +0300

    Working task6 (optimal method)

--- LAB1_semester2.cpp
+++ LAB1_semester2.cpp
@@ -7,20 +7,7 @@
 
 using namespace std;
 
-// https://neerc.ifmo.ru/wiki/index.php?title=%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%9B%D0%B0%D0%BD%D0%B4%D0%B0%D1%83-%D0%92%D0%B8%D1%88%D0%BA%D0%B8%D0%BD%D0%B0_(k_%D1%80%D0%B0%D0%B7%D0%BB%D0%B8%D1%87%D0%B8%D0%B9)
-
 int main()
 {
-    string x("father");
-    string y(R"(In entirely be to at settling felicity. Fruit two match men you seven share. Needed as or is enough points. Miles at smart ﻿no marry whole linen mr. Income joy nor can wisdom summer. Extremely depending he gentleman improving intention rapturous as.
-
-        Bed sincerity yet therefore forfeited his certainty neglected questions.Pursuit chamber as elderly amongst on.Distant however warrant farther to of.My justice wishing prudent waiting in be.Comparison age not pianoforte increasing delightful now.Insipidity sufficient dispatched any reasonably led ask.Announcing if attachment resolution sentiments admiration me on diminution.)");
-
-    auto res = string_algorithms::task2_k_differences(y, x, 2);
-
-    cout << x << "\n" << y << "\n\n";
-    for (auto index : res)
-    {
-        cout << index << "\n";
-    }
+    cout << string_algorithms::task6_optimal("Lorem ipsum dolor sit amet, consectetuer amdipiscing elit.");
 }
--- task6/code.h
+++ task6/code.h
@@ -1,7 +1,7 @@
 #pragma once
-
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -9,7 +9,34 @@ namespace string_algorithms
 {
 	string task6_optimal(const string& s)
 	{
-		//vector<>
-		return s;
+        // Precomputing stage
+        suffixtree::tree buff(s + UNIQUE_CHAR_1);
+        vector<suffixtree::node>& nodes = buff.nodes;
+
+        // Actual computing
+        string max_s = "";
+
+        queue<pair<int, string>> q;
+        q.push({ 0, "" });
+        while (!q.empty())
+        {
+            auto f = q.front();
+            q.pop();
+            int node = f.first;
+            string node_str = f.second;
+            bool is_inner = false;
+            for (auto it : nodes[node].children)
+            {
+                if (it.second == -1) continue;
+                is_inner = true;
+                suffixtree::node& cur = nodes[it.second];
+                q.push({ it.second, node_str + s.substr(cur.l, cur.r - cur.l) });
+            }
+
+            if (is_inner && node_str.size() > max_s.size())
+                max_s = node_str;
+        }
+
+		return max_s;
 	}
 }
