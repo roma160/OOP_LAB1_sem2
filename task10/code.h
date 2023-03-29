c67124f31c6574fa2c54d6a43918a222f9ddd1d6
super mario <mario160@mail.ru>
2023-03-29 05:05:47 +0300

    Backing up the progress

--- task10/code.h
+++ task10/code.h
@@ -1,14 +1,41 @@
 #pragma once
-#include <string>
+
 #include <vector>
 #include <queue>
+
+#include "deps/list_sort.h"
 
 using namespace std;
 
 namespace string_algorithms
 {
-	void task10(const string& s)
+	int task10(const vector<int>& list, int to_find, int guess_index)
 	{
+		// Prepatations
+		vector<pair<int, int>> indexed_list = sort_list(list);
+		for(int i = 0; i < indexed_list.size(); i++)
+			if(indexed_list[i].second == guess_index)
+			{
+				guess_index = i;
+				break;
+			}
 
+		// Exit on wrong input
+		if (to_find < indexed_list[0].first || to_find > indexed_list[indexed_list.size() - 1].first)
+			return -1;
+		if (guess_index < 0 || guess_index >= indexed_list.size())
+			return _raw_task8(indexed_list, to_find);
+
+		// Algorithm starts
+		int l = guess_index, r = guess_index;
+		int inc = 1;
+		if(to_find >= indexed_list[r].first)
+			while (to_find >= indexed_list[r].first)
+			{
+				r += inc;
+				if(r >= indexed_list.size())
+					return _raw_task8(indexed_list, to_find);
+
+			}
 	}
 }
--- task8/code.h
+++ task8/code.h
@@ -9,9 +9,9 @@ using namespace std;
 
 namespace string_algorithms
 {
-	int _raw_task8(const vector<pair<int, int>>& indexed_list, int to_find, int l = 0, int r = -1)
+	int _raw_task8(const vector<pair<int, int>>& indexed_list, int to_find)
 	{
-		if (r == -1) r = indexed_list.size() - 1;
+		int l = 0, r = indexed_list.size() - 1;
 		while (true)
 		{
 			int middle = (l + r) / 2;
@@ -23,9 +23,9 @@ namespace string_algorithms
 		}
 	}
 
-	int task8(const vector<int>& list, int to_find, int l = 0, int r = -1)
+	int task8(const vector<int>& list, int to_find)
 	{
 		vector<pair<int, int>> indexed_list = sort_list(list);
-		return _raw_task8(indexed_list, to_find, l, r);
+		return _raw_task8(indexed_list, to_find);
 	}
 }
