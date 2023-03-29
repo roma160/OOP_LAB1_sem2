904621222f261937b148893474710c7c0d37f06a
super mario <mario160@mail.ru>
2023-03-29 04:14:55 +0300

    Task8 completed

--- LAB1_semester2.cpp
+++ LAB1_semester2.cpp
@@ -9,8 +9,6 @@ using namespace std;
 
 int main()
 {
-    for (auto el : string_algorithms::task7(
-        { 3, 4, 8, 4, 0, 9, 1, 2, 8, 3, 0, 2, 1 },
-        { 3, 8, 9, 0, 2, 4, 8, 3, 0, 2, 9, 4, 8 }
-    )) cout << el << " ";
+    cout << string_algorithms::task8(
+        { 3, 4, 8, 4, 0, 9, 1, 2, 8, 3, 0, 2, 1 }, 8);
 }
